fe2af2ff761853b18b014d1793ccfa96d9d80bbc
Lukas de Oliveira Biscaro <lukas.biscaro@hotmail.com>
2022-10-28 23:04:28 -0300

    3 - Commit
    Added some more animations and started coding the score counter and game over information.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,13 @@
 const metamask = document.querySelector('.metamask')
 const hacker = document.querySelector('.hacker')
-const spanPlayer = document.querySelector('.player-username')
+const span = document.querySelector('.player-username')
+const cloud = document.querySelector('.cloud')
+
+window.onload = () => {
+    const playerUserName = localStorage.getItem('playerUserName')
+    span.innerHTML = playerUserName.toUpperCase()
+
+}
 
 const jump = () => {
     metamask.classList.add('jump')
@@ -12,20 +19,31 @@ const jump = () => {
 const loop = setInterval(() => {
     const hackerPosition = hacker.offsetLeft
     const metamaskPosition = Number(window.getComputedStyle(metamask).bottom.replace('px', ''))
+    const cloudPosition = cloud.offsetLeft
 
     if (hackerPosition <= 109 && hackerPosition > 0 && metamaskPosition < 74) {
+
         hacker.style.animation = 'none'
         hacker.style.left = `${hackerPosition}px`
 
         metamask.style.animation = 'none'
         metamask.style.bottom = `${metamaskPosition}px`
+
+        cloud.style.animation = 'none'
+        cloud.style.left = `${cloudPosition}px`
+
+        counter = -1
+
+        // game-over class começa com atributo disabled, conforme o if de batida é executado, "removeAttribute" retira o atributo disable mostrando o css game-over
     }
 }, 0);
 
-window.onload = () => {
-    const playerUserName = localStorage.getItem('playerUserName')
-    spanPlayer.innerHTML = playerUserName.toUpperCase()
+let counter = 0
 
+const score = () => {
+    counter++ 
+    console.log(counter)
 }
 
+document.addEventListener('keydown', score)
 document.addEventListener('keydown', jump)
